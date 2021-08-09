b5f5b69839d113b75083a8f7357b609443909c93
Geethpriya <geethpriya.r@gmail.com>
2021-08-09 10:07:49 -0400

    this is to test dev branch

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# This is to test the git learning 
 
 def index(request):
     return HttpResponse("Hello, world. This is my test page from testapp1!!! - version 2")
