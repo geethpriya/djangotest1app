856d45682b943c7179dcb13f2a840a89ce1c73aa
Geethpriya <geethpriya.r@gmail.com>
2021-08-09 10:38:02 -0400

    third commit

--- myapp/views.py
+++ myapp/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 
 # This is to test the git learning 
 # I am in test brach
+# My third commit
 def index(request):
     return HttpResponse("Hello, world. This is my test page from testapp1!!! - version 2")
